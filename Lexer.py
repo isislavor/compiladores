925c658544d068b92ef1eaabc6ce7fc592e925d6
Isis Lavor <isisnascimento142@gmail.com>
2025-05-15 23:36:15 -0300

    Reconhecimento de inteiros e float

--- Lexer.py
+++ Lexer.py
@@ -1,64 +1,54 @@
-from Consts import Consts 
-from Token import Token 
-from Error import Error 
-
+from Consts import Consts
+from Token import Token
+from Error import Error
 class Lexer:
-    def __init__(self, source_code):
-        self.code = source_code
-        self.current = None
-        self.indice, self.coluna, self.linha = -1, -1, 0
+  def __init__(self, source_code) -> None:
+    self.code = source_code
+    self.current = None
+    self.indice, self.coluna, self.linha = -1, -1, 0
+    self.__advance()
+
+  def __advance(self):
+    self.__advanceCalc()
+    self.current = self.code[self.indice] if self.indice < len(self.code) else None
+
+  def __advanceCalc(self):
+    self.indice += 1
+    self.coluna += 1
+    if self.current == '\n': 
+      self.coluna = 0
+      self.linha += 1      
+    return self
+
+  def makeTokens(self):
+    tokens = []
+    while self.current != None:
+      if self.current in ' \t':
         self.__advance()
+      if self.current == Consts.PLUS:
+        tokens.append(Token(Consts.PLUS,Consts.PLUS))  
+        self.__advance()
+      elif self.current in Consts.DIGITOS + '.':
+        tokens.append(self.makeNumber())
+      else:
+        return tokens, Error("Erro léxico")
+    tokens.append(Token(Consts.EOF))
+    return tokens, None
 
-    def __advance(self):
-        self.indice += 1
-        if self.indice < len(self.code):
-            self.current = self.code[self.indice]
+  def makeNumber(self):
+    num_str = ''
+    dot_count = 0
+    start_col = self.coluna
 
-            if self.current == '\n':
-                self.linha += 1
-                self.coluna = 0
-            else:
-                self.coluna += 1
+    while self.current is not None and (self.current.isdigit() or self.current == '.'):
+        if self.current == '.':
+            if dot_count == 1: break
+            dot_count += 1 
+            num_str += '.'
         else:
-            self.current = None
-            
-        self.__advanceCalc(self.current)
-
-# Só falta fazer os outros e entender o porquê da chamada do advanceCalc nesse método acima!!!!
-
-    def __advanceCalc(self, _char=None):
-        return self
-    
-    def makeTokens(self):
-        tokens = []
-
-        while self.current != None:
-            if self.current in '\t':
-                self.__advance()
-            elif self.current.isdigit():
-                tokens.append(self.__makeNumber())
-            else:
-                self.__advance()
-
-        tokens.append(Token(Consts.EOF))
-        return tokens, None
-
-    def __makeNumber(self):
-        num_str = ''
-        dot_count = 0
-        start_col = self.coluna
-
-        while self.current is not None and (self.current.isdigit() or self.current == '.'):
-            if self.current == '.':
-                if dot_count == 1:
-                    break
-                dot_count += 1
-                num_str += '.'
-            else:
-                num_str += self.current
-            
-            self.__advance()
-        if dot_count == 0:
-            return Token(Consts.INT, int(num_str), self.linha, start_col)
-        else:
-             return Token(Consts.FLOAT, float(num_str), self.linha, start_col)              
+            num_str += self.current
+        self.__advance()
+    if dot_count == 0:
+        return Token(Consts.INT, int(num_str))
+    else:
+          return Token(Consts.FLOAT, float(num_str))             
--- Token.py
+++ Token.py
@@ -1,15 +1,12 @@
+####################################### TOKENS #######################################
 class Token:
-    def __init__(self, tipo, valor=None, linha=None, coluna=None):
+    def __init__(self, tipo, valor=None):
         self.type = tipo
         self.value = valor
-        self.linha = linha
-        self.coluna = coluna
 
     def matches(self, _type, _value):
         return self.type == _type and self.value == _value
 
     def __repr__(self):
-        pos_info = f' @L{self.linha},C{self.coluna}' if self.linha is not None else ''
-        if self.value is not None:
-            return f'{self.type}:{self.value}{pos_info}'
-        return f'{self.type}{pos_info}' 
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
